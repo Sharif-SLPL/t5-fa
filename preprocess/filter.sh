82d2d1c1fb6f0b59329fa7450a698f759af41363
Sadra Sabouri <43045767+sadrasabouri@users.noreply.github.com>
2022-08-06 16:59:04 +0430

    add : `filter.sh` added.
    This file is simply the core of our preprocess code.

---
+++ preprocess/filter.sh
@@ -0,0 +1,21 @@
+#   This file uses `sed` to filter non-proper characters in a streamin manner.
+#   Use it like below:
+#       bash filter.sh raw.txt
+#   =========================================================================
+
+#   filter non-farsi characters:
+sed 's/[^<200c>ۀﺀ-ﻍ ﻑ-ﻱ ًھﮒﮋﭙﭼۆۇێەﯼ!.,?؟!،]/ /g' $1 | \
+
+#   unify characters:
+sed 's/[ﻱ­ێ]/ﯼ/g' - | \
+sed 's/[ۀﺓ]/ﻩ/g' - | \
+sed 's/[ﻙ]/ﮎ/g' - | \
+sed 's/[ﺇ]/ﺍ/g' - | \
+sed 's/[ڒ]/ﺭ/g' - | \
+sed 's/[ۆ]/ﻭ/g' - | \
+
+#   unifiy spaces to a space:
+sed 's/  */ /g' - | \
+
+#   remove empty lines:
+sed -r '/^\s*$/d' -
